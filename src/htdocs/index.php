7ca31adbfc159489802855e4014372a6691fcbc2
Alexei Yuzhakov <ayuzhakov@plesk.com>
2021-06-01 15:03:58 +0700

    TECH Context initialization is made by default in Plesk Obsidian

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,9 +1,5 @@
 <?php
 // Copyright 1999-2021. Plesk International GmbH.
 
-$moduleId = basename(dirname(__FILE__));
-
-pm_Context::init($moduleId);
-
 $application = new pm_Application();
 $application->run();
